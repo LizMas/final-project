793bb407bfe2db44a2a1d75ea94c930349c29169
LizMas <lizmasten@g.harvard.edu>
2019-11-19 21:53:10 -0500

    Shiny tabs done

--- final_proj_shiny_app/app.R
+++ final_proj_shiny_app/app.R
@@ -11,50 +11,99 @@ library(shinythemes)
 library(shinyBS)
 library(tidyverse)
 
-ui <- fluidPage(
-               # h2(textOutput("currentTime")),
-  
-  
-    navbarPage("Fatalities in the Yemeni Civil War, Summer 2019",
-               theme = shinytheme("flatly"),
-               position = "static-top",
-               
+ui <- fluidPage(theme = shinytheme("flatly"),
+                position = "static-top",
+                
+                br(),
+                
+    navbarPage("Conflict in the Yemeni Civil War, Summer 2019",
+          
+    #show pop-up about
+    bsModal(
+        id = "tutorialModal",
+        title = "",
+        trigger = "",
+        #img(src = "whatever-image.jpg",
+        # style = "display: block; margin-left: auto; margin-right: auto;",
+        #height = "120",
+        #width = "120"),
+      htmlOutput("tutorial")),
+              tabPanel("Fatalities",
+                 titlePanel("Fatalities in the Yemeni Civil War, Summer 2019"),
+                 imageOutput("map")),    
+    
+    
+    #here's where the two interactive maps go
+    
+    navbarMenu("Interactive Maps",
+               #this is the fat by actor drop down (1) 
+               tabPanel("Fatalities by Actor",
+                        titlePanel(
+                          textOutput("FatalitiesbyactorTitle")
+                        ),
+                        sidebarLayout(position = "right",
+                                      sidebarPanel(
+                                        selectInput("recode_actor1",
+                                                    "Actor:",
+                                              choices = sort(unique(map_fat_actor1$fat_actor1)),
+                                              selected = "Pro-Goverment Militias")),
+                                      mainPanel(
+                                        plotOutput("map_fat_actor1.rds")
+                                        
+                                      )
+                        )
+               ),
+    #this is the fat by event type dropdown (2)
+    
+    tabPanel("Fatalities by Event Type",
+             titlePanel(
+               textOutput("FatalitiesbyeventTitle")
+             ),
+             sidebarLayout(position = "right",
+                           sidebarPanel(
+                             selectInput("event_type",
+                                         "Event Type:",
+                                         choices = sort(unique(map_fat_actor1$fat_actor1)),
+                                         selected = "Air/drone strike")),
+                           mainPanel(
+                             plotOutput("map_fat_actor1.rds")
+                             
+                           )
+             )
+    ))))
 
-               #I want to create a popup window, but that's not working
-               #bsModal(
-                   #id = "tutorialModal",
-                #   title = "",
-                 #  trigger = "",
-                   #img(src = "unhcr_logo.jpg",
-                  # style = "display: block; margin-left: auto; margin-right: auto;",
-                   #height = "120",
-                   #width = "120"),
-               #htmlOutput("tutorial")
-    #),
-    #titlePanel(
-     # textOutput("destTitle")
-    #),
-               tabPanel("Running map",
-                        imageOutput("map")),
-               tabPanel("Interactive maps",
-                       imageOutput("image")),
-                tabPanel("Regression stuff",
-                         imageOutput("imagereg")),
-                tabPanel("About",
-                         #do I save an object for about text and then call it here? How do I do that? 
-                         h5("about text"))))
+
+
+
+              # tabPanel("Fatalities by Event Type"),
+               # imageOutput("map_fat_actor1")),
+                        
+              # tabPanel("Fatalities by Actor"),
+              #tabPanel("Analytics"))
+                       #imageOutput("image"))))
+  
 
 
 server <- function(input, output, session) {
-  #attempt to create a timer - need to figure out how to create timer from specific date
-  output$currentTime <- renderText({
-    invalidateLater(1000, session)
-    paste("The War in Yemen has been going on for:", Sys.time())
+  toggleModal(session, "tutorialModal", toggle = "open")
+  
+  #This is for the pop-up 
+  output$tutorial <- renderText({
+    HTML("Text for About Page goes here"
+         
+    )
   })
-  #this is where map will go 
+  #this is where map goes 
     output$map <- renderImage({
         list(src = "map.gif",
-             contentType = "image/gif")}, deleteFile = FALSE)}
+             contentType = "image/gif")}, deleteFile = FALSE)
 
+
+output$map_fat_actor1 <- renderImage({
+  list(src = "map_fat_actor1.rds",
+       contentType = "image/gif")}, deleteFile = FALSE)
+}
 
 shinyApp(ui, server)
+
+
