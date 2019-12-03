e26726ab2aa52221b26626464248b2a9c48560da
LizMas <lizmasten@g.harvard.edu>
2019-12-02 20:41:51 -0500

    tabs fixed; fatalities by year graph loaded in

--- final_proj_shiny_app/app.R
+++ final_proj_shiny_app/app.R
@@ -9,7 +9,145 @@ library(transformr)
 library(lubridate)
 library(shinythemes)
 library(shinyBS)
+library(ggridges)
+library(plotly)
+library(png)
+library(leaflet)
 library(tidyverse)
+
+locate_all <- read_rds("clean-data_2/locations.rds")
+
+shap <- read_rds("clean-data_2/shape.rds")
+
+fat_sub_event <- locate_all %>% 
+  select(sub_event_type, fatalities) %>% 
+  dplyr::group_by(sub_event_type) %>% 
+  summarise(sum_fatby_event = sum(fatalities)) %>% 
+  filter(sum_fatby_event != 0)
+
+map_fat_sub_event <- ggplot(shap) +
+  geom_sf(data = shap) +
+  geom_sf(data = fat_sub_event, aes(color = sub_event_type))
+
+#recode actor1 
+
+locate_all$actor1 <- as.factor(locate_all$actor1)
+
+conflict_actor1_recode <- locate_all %>% 
+  mutate(recode_actor1 = fct_recode(actor1,
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2016-) Supreme Political Council",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-)",
+                                    "Saudi Coalition Operations" = "Operation Restoring Hope",
+                                    "Unidentified Militias" = "Unidentified Armed Group (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Security Belt Forces",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Hadhrami Elite Forces",
+                                    "Unidentified Militias" = "Unidentified Tribal Militia (Yemen)",
+                                    "Pro-Government Militias" = "Hujur Tribal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2015-2016) Supreme Revolutionary Committee",
+                                    "Pro-Government Militias" = "National Resistance Forces",
+                                    "Pro-Government Militias" = "Popular Resistance",
+                                    "Separatist Militias" = "Southern Resistance", 
+                                    "Saudi Coalition Operations" = "Operation Decisive Storm",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Security Belt Forces",
+                                    "Pro-Government Militias" = "Militia (Abu al Abbas)", 
+                                    "Military Forces of Yemen" = "Unidentified Military Forces",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 35th Armored Brigade",
+                                    "Police Forces of Yemen" = "Police Forces of Yemen (2012-)",
+                                    "Pro-Government Militias" = "Bilharith Tribal Militia (Yemen)",
+                                    "Separatist Militias" = "Houthi Movement - Ansar Allah", 
+                                    "Pro-Government Militias" = "Militia (Pro-Government)",
+                                    "Pro-Government Militias" = "Al Humayqani Tribal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Shabwani Elite Forces",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2016-) Republican Guard",
+                                    #Anwar al-Awlaki 
+                                    "Al-Qaeda and Affiliates" = "Al Awlaki Tribal Militia (Yemen)",
+                                    "Al-Qaeda and Affiliates" = "AQAP: Al Qaeda in the Arabian Peninsula	",
+                                    #STC alligned
+                                    "Separatist Militias" = "Subaihi Tribal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Presidential Guard",
+                                    "Military Forces of Yemen", "Military Forces of Yemen (2012-) Border Guard",
+                                    "Police Forces of Yemen", "Police Forces of Yemen (2015-2016) Special Security Forces",
+                                    "Military Forces of Yemen", "Military Forces of Yemen (2012-) 17th Infantry Brigade",
+                                    "Pro-Government Militias" = "Al Islah Party",
+                                    "Pro-Government Militias" = "Giants Brigade",
+                                    "Islamic State and Affiliates" = "Ansar al Sharia (Yemen)",
+                                    "Islamic State and Affiliates" = "Islamic State (Yemen)",
+                                    "Pro-Government Militias" = "Tihama Resistance",
+                                    "Police Forces of Yemen" = "Police Forces of Yemen (2016-) Supreme Political Council",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 22nd Armored Brigade",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 30th Brigade",
+                                    "Pro-Government Militias" = "Militia (Hamoud Saeed al Mikhlafi)",
+                                    "Protesters/ Rioters" = "Rioters (Yemen)",
+                                    "Protesters/ Rioters" = "Protesters (Yemen)",
+                                    "Unidentified Militias" = "Al al Amir Tribal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 170th Air Defence Brigade",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Saiqa Brigades",
+                                    #was UAE backed before withdrawal 
+                                    "Separatist Militias" = "Southern Movement",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2015-2016) Republican Guard", 
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 102nd Brigade",
+                                    #aligned with Hajouri against Houthis as of Mar 2019
+                                    "Pro-Government Militias" = "Murad Tribal Militia (Yemen)",
+                                    "Unidentified Militias" = "Unidentified Communal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Lawdar Communal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Azal Resistance",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Political Security Organisation", 
+                                    #Saudi-backed, Salafi, likely AQAP affiliated 
+                                    "Pro-Government Militias" = "Militia (Al Mihdhar)",
+                                    "Unknown Affiliation" = "Bani Khawlah Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Riyam Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Abyadh Communal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Ghanam Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Jalal Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Jalal Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Jaradi Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Udhr Tribal Militia (Yemen)",
+                                    "Unknown Affiliation" = "Al Warawirah Tribal Militia (Yemen)",	
+                                    "Unknown Affiliation" = "Ba Awdah Tribal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 63rd Brigade",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 9th Brigade",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Coast Guard",
+                                    "Unknown Affiliation" = "Militia (Mukhtar al Zurayqi)",
+                                    "Unknown Affiliation" = "Ubaydah Tribal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Support and Reinforcement Brigade",
+                                    "Unknown Affiliation" = "As Silah Communal Militia (Yemen)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) 4th Brigade",
+                                    "Unknown Affiliation" = "Al Hussein bin Mohammed Tribal Militia (Yemen)",
+                                    #I stopped with the Unknown Affiliations here because there were 20 more with only 1 reported fatality each 
+                                    "Military Forces of Yemen" = "MIlitary Forces of Yemen (2012-)",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Counter-Terrorism Unit",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Republican Guard",
+                                    "Police Forces of Yemen" = "Police Forces of Yemen (2015-2016) Prison Guards",
+                                    "Military Forces of Yemen" = "Military Forces of Yemen (2012-) Tihami Elite Forces",
+                                    "Police Forces of Yemen" = "Police Forces of Yemen (2016-) Prison Guards",
+                                    "Police Forces of Yemen" = "Police Forces of Yemen (2016-) Special Security Forces" ))
+
+#find total fatalities by actor1
+
+fat_actor1 <- conflict_actor1_recode %>% 
+  select(recode_actor1, event_date, fatalities) %>% 
+  dplyr::group_by(recode_actor1) %>% 
+  summarise(sum_fatby_actor1 = sum(fatalities)) %>% 
+  filter(sum_fatby_actor1 != 0) %>% 
+  arrange(desc(sum_fatby_actor1)) %>% 
+  #head 9 because that's where it stops getting interesting number-wise
+  head(9)
+
+map_fat_actor1 <- ggplot(shap) +
+  geom_sf(data = shap) +
+  geom_sf(data = fat_actor1, aes(color = recode_actor1))
+
+#for toggle jitter plot 
+
+xx <- fat_actor1 %>% 
+  filter(recode_actor1 %in% c("Military Forces of Yemen", "Saudi Coalition Operations", "Pro-Government Militias", "Unidentified Militias", "Islamic State and Affiliates", "Sepratist Militias", "Unidentified Militias")) %>% 
+  group_by(recode_actor1)
+
+#load in the .png for fig1
+readPNG(file.path("fig1.png"))
+
+ 
+#shiny app starts here --------------------------------
 
 ui <- fluidPage(theme = shinytheme("flatly"),
                 position = "static-top",
@@ -19,7 +157,8 @@ ui <- fluidPage(theme = shinytheme("flatly"),
     
               tabPanel("Fatalities",
                  titlePanel("Fatalities in the Yemeni Civil War, Summer 2019"),
-                 imageOutput("map")),    
+                 imageOutput("map"),
+                 imageOutput("fig_fatalities")),  
     
     
     #here's where the two interactive maps go
@@ -35,18 +174,18 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                               choices = sort(unique(fat_actor1$recode_actor1)),
                                               selected = "Pro-Goverment Militias")),
                                       mainPanel(
-                                        plotOutput("fat_actor1")
+                                        plotOutput("fat_actor1"),
+                                        plotOutput("jitter_actor")
                                         
                                       )
                         )),
     #this is the fat by event type dropdown (2)
     
     tabPanel("Fatalities by Event Type",
-             titlePanel("Fatalities by Event Type")
-             ),
+             titlePanel("Fatalities by Event Type"),
              sidebarLayout(position = "right",
                            sidebarPanel(
-                             selectInput("fat_sub_event",
+                             selectInput("map_fat_sub_event",
                                          "Event Type:",
                                          choices = sort(unique(fat_sub_event$sub_event_type)),
                                          selected = "Air/drone strike")),
@@ -54,7 +193,8 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                              plotOutput("fat_sub_event")
                              
                            )
-             )),
+             ))),
+
     
     #regression will go here: 
 tabPanel("Analysis"),
@@ -86,6 +226,14 @@ server <- function(input, output, session) {
     output$map <- renderImage({
         list(src = "map.gif",
              contentType = "image/gif")}, deleteFile = FALSE)
+    
+    #this is a plot, just uploading an image of a graph for ease
+    
+    output$fig_fatalities <- renderImage({
+      filename <- "fig1.png"
+      list(src = filename)
+    }, deleteFile = FALSE)
+
 
     #this is where map_fat_by_actor1 goes
 
@@ -97,11 +245,16 @@ output$fat_actor1 <- renderPlot({
   
 })
 
+
+#ggplot vs plot_ly- watch datacamp 
 output$fat_sub_event <- renderPlot({
   map_fat_sub_event <- ggplot(shap) +
     geom_sf(data = shap) +
     geom_sf(data = fat_sub_event, aes(color = sub_event_type, fill = sub_event_type))
   map_fat_sub_event
+  
+ # ggplotly(map_fat_sub_event)
+  #map_fat_sub_event
   
 })
 
